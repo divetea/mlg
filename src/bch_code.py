424c144458087751ae738f1a5190d09bfec0c78f
Niklas Förster <niklasf@linux.fritz.box>
2019-02-09 12:52:31 +0100

    refactor and add gamma and roh

--- src/bch_code.py
+++ src/bch_code.py
@@ -13,7 +13,7 @@ def expon_to_int(exponents):
     return result
 
 
-class BCH_Code(object):
+class BCHCode(object):
     """This is a representation of a BCH-code."""
 
     def __init__(self, n, h):
@@ -21,78 +21,10 @@ class BCH_Code(object):
         self._n = n
         # self._generator_pol = generator_pol
         self._h = h
-        self._indexes_k = None
-        self._indexes_n = None
-
-    @property
-    def n(self):
-        """Getter for n."""
-        # print("Getting value n")
-        return self._n
-
-    @n.setter
-    def n(self, value):
-        """Setter for n."""
-        # print("Setting value n")
-        self._n = value
-
-    # @property
-    # def generator_pol(self):
-    #     """Getter for generator_pol."""
-    #     # print("Getting value generator_pol")
-    #     if isinstance(self._generator_pol, str):
-    #         self.generator_pol = int(self._generator_pol, 2)
-    #     return self._generator_pol
-    #
-    # @generator_pol.setter
-    # def generator_pol(self, value):
-    #     """Setter for generator_pol."""
-    #     if isinstance(value, str):
-    #         value = int(value, 2)
-    #     # print("Setting value generator_pol")
-    #     self._generator_pol = value
-
-    @property
-    def h(self):
-        """Getter for h."""
-        if isinstance(self._h, str):
-            self.h = int(self._h, 2)
-        # print("getting h")
-        return self._h
-
-    @h.setter
-    def h(self, value):
-        """Setter for h."""
-        if isinstance(value, str):
-            value = int(value, 2)
-        # print("Setting value h to", value)
-        self._h = value
-
-    @property
-    def indexes_k(self):
-        """Getter for indexes_k."""
-        if self._indexes_k is None:
-            self.indexes_k = self._generate_indexes_k(self._n, self.h)
-        return self._indexes_k
-
-    @indexes_k.setter
-    def indexes_k(self, value):
-        """Setter for indexes_k."""
-        # print("Setting value indexes_k to", value)
-        self._indexes_k = value
-
-    @property
-    def indexes_n(self):
-        """Getter for indexes_n."""
-        if self._indexes_n is None:
-            self.indexes_n = self._generate_indexes_n(self._n, self.h)
-        return self._indexes_n
-
-    @indexes_n.setter
-    def indexes_n(self, value):
-        """Setter for indexes_n."""
-        # print("Setting value indexes_n to", value)
-        self._indexes_n = value
+        self._indexes_k = self._generate_indexes_k(self.n, self.h)
+        self._indexes_n = self._generate_indexes_n(self.n, self.h)
+        self._roh = len(self.indexes_k)
+        self._gamma = len(self.indexes_n)
 
     def _generate_indexes_k(self, n, h):
         bin_h = bin(h)[2:]
@@ -119,3 +51,89 @@ class BCH_Code(object):
                     result[i].append(j)
 
         return np.array(result, dtype=np.int16)
+
+    @property
+    def n(self):
+        """Getter for n."""
+        # print("Getting value n")
+        return self._n
+
+    @n.setter
+    def n(self, value):
+        """Setter for n."""
+        # print("Setting value n")
+        self._n = value
+
+    @property
+    def h(self):
+        """Getter for h."""
+        if isinstance(self._h, str):
+            self.h = int(self._h, 2)
+        # print("getting h")
+        return self._h
+
+    @h.setter
+    def h(self, value):
+        """Setter for h."""
+        if isinstance(value, str):
+            value = int(value, 2)
+        # print("Setting value h to", value)
+        self._h = value
+
+    @property
+    def indexes_k(self):
+        """Getter for indexes_k."""
+        return self._indexes_k
+
+    @indexes_k.setter
+    def indexes_k(self, value):
+        """Setter for indexes_k."""
+        # print("Setting value indexes_k to", value)
+        self._indexes_k = value
+
+    @property
+    def indexes_n(self):
+        """Getter for indexes_n."""
+        return self._indexes_n
+
+    @indexes_n.setter
+    def indexes_n(self, value):
+        """Setter for indexes_n."""
+        self._indexes_n = value
+
+    @property
+    def gamma(self):
+        """Getter for gamma."""
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, value):
+        """Setter for gamma."""
+        self._gamma = value
+
+    @property
+    def roh(self):
+        """Getter for roh."""
+        return self._roh
+
+    @roh.setter
+    def roh(self, value):
+        """Setter for roh."""
+        self._roh = value
+
+
+    # @property
+    # def generator_pol(self):
+    #     """Getter for generator_pol."""
+    #     # print("Getting value generator_pol")
+    #     if isinstance(self._generator_pol, str):
+    #         self.generator_pol = int(self._generator_pol, 2)
+    #     return self._generator_pol
+    #
+    # @generator_pol.setter
+    # def generator_pol(self, value):
+    #     """Setter for generator_pol."""
+    #     if isinstance(value, str):
+    #         value = int(value, 2)
+    #     # print("Setting value generator_pol")
+    #     self._generator_pol = value
--- tests/test_bch_code.py
+++ tests/test_bch_code.py
@@ -2,7 +2,7 @@
 """Test module for bch_code.py."""
 
 import unittest
-from src.bch_code import (BCH_Code, expon_to_int)
+from src.bch_code import (BCHCode, expon_to_int)
 
 
 class TestExponToInt(unittest.TestCase):
@@ -17,15 +17,31 @@ class TestExponToInt(unittest.TestCase):
 class TestBCHCode(unittest.TestCase):
     """Test case for the BCHCode object."""
 
+    def test_zero(self):
+        """Test BCHCode(0, 0)."""
+        bch = BCHCode(0, 0)
+        self.assertEqual(bch.indexes_k.tolist(), [])
+        self.assertEqual(bch.indexes_n.tolist(), [])
+
     def test_small1(self):
         """Test BCHCode(3, 2)."""
-        bch = BCH_Code(3, 2)
+        bch = BCHCode(3, 2)
         self.assertEqual(bch.indexes_k.tolist(), [[0], [1], [2]])
         self.assertEqual(bch.indexes_n.tolist(), [[0], [1], [2]])
 
     def test_small2(self):
         """Test BCHCode(4, 3)."""
-        bch = BCH_Code(4, 3)
+        bch = BCHCode(4, 3)
+        self.assertEqual(
+            bch.indexes_k.tolist(),
+            [[0, 1], [1, 2], [2, 3], [0, 3]])
+        self.assertEqual(
+            bch.indexes_n.tolist(),
+            [[0, 3], [0, 1], [1, 2], [2, 3]])
+
+    def test_construct_with_str(self):
+        """Test BCHCode(4, '0b0011')."""
+        bch = BCHCode(4, '0b0011')
         self.assertEqual(
             bch.indexes_k.tolist(),
             [[0, 1], [1, 2], [2, 3], [0, 3]])
